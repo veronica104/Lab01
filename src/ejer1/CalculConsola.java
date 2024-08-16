839955f89b6faec8ddde287935ba205ed8f3042e
veronica104 <isabelduarte413@gmail.com>
2024-08-16 17:45:57 -0500

    entrega 3
    en esta entrega se encuentra la calculadora de consola completa, con cada una de sus funciones (suma, resta, multiplicación, divición, funciones trigonometricas, raiz, potencia).

--- src/ejer1/CalculConsola.java
+++ src/ejer1/CalculConsola.java
@@ -16,7 +16,10 @@ public class CalculConsola {
             System.out.println("2.resta");
             System.out.println("3.multiplicacion");
             System.out.println("4.division");
-            System.out.println("5.salir");
+            System.out.println("5.funciones trigonometrics");
+            System.out.println("6.raiz");
+            System.out.println("7.potencia");
+            System.out.println("8.salir");
             System.out.println("ingrese su opcion: ");
             opc = scanner.nextInt();
             
@@ -57,14 +60,74 @@ public class CalculConsola {
                     int divi =num1/num2;
                     System.out.println("la division es :"+divi);
                 }
-                case 5 ->{
+                case 5 -> {
+                    int opci=0;
+                    double angulo;
+                    do {
+                     System.out.println("-----funciones trigonometricas----");
+                     System.out.println("1.seno");
+                     System.out.println("2.coseno");
+                     System.out.println("3.tangente");
+                     System.out.println("4.salir");
+                     System.out.println("ingrese su opcion");
+                     opci = scanner.nextInt();
+                   
+                     
+                     switch(opci){
+                         case 1->{
+                             System.out.println("ingrese el numero :");
+                             angulo = scanner.nextDouble();
+                             System.out.println("el seno es: "+Math.sin(angulo));
+                             break;
+                         }
+                          case 2->{
+                             System.out.println("ingrese el numero :");
+                             angulo = scanner.nextDouble();
+                             System.out.println("el coseno es: "+ Math.cos(angulo));
+                             break;
+                          }
+                          case 3->{
+                             System.out.println("ingrese el numero :");
+                             angulo = scanner.nextDouble();
+                             System.out.println("el tangente es: " + Math.tan(angulo));
+                             break;
+                          }
+                          case 4->{
+                             System.out.println("salir");
+                             break;
+                          }
+                        default -> 
+                        System.out.println("Opcion invalida.");
+                     }     
+                   
+                     }while(opci !=4);
+             }        
+                          
+                case 6->{
+                    System.out.println("Raiz");
+                    System.out.println("ingrese el numero :");
+                    double numero = scanner.nextDouble();
+                    System.out.println("ingrese el indice de la raiz:");
+                    double indice = scanner.nextDouble();
+                    double raize = Math.pow(numero, 1/indice);
+                    System.out.println("la raiz" + indice + "enesima de " + numero + "es: " + raize );
+                }
+                case 7 ->{
+                    System.out.println("potencia");
+                    System.out.println("ingrese la base :");
+                    double base = scanner .nextDouble();
+                    System.out.println("ingrese el exponente :");
+                    int expo = scanner.nextInt();
+                    double resul = Math.pow(base, expo);
+                    System.out.println(base + "elevado a la" + expo + "es" + resul);
+                }
+                case 8 ->{
                     System.out.println("Saliendo del programa...");
                 }
                  default -> 
                  System.out.println("Opcion invalida.");
             }
-        }while (opc != 5);
+        }while (opc != 8);
        scanner.close();
      }
-}
-    
+}5
